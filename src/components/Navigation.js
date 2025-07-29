9d2a2686b8a4582d326076bff3ca94b102cf5ae7
unknown <ashraful.cse.20230104030@aust.edu>
2025-07-29 23:30:25 +0600

    update in Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,25 +3,37 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
-
-function Navigation() {
+import styles from './Navigation.module.css'; 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className={styles.navbar}>
       <Container>
-        <Navbar.Brand as={Link} to="/">To-Do App</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className={styles.brand}>
+          To-Do App
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to ="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            //<NavDropdown title="Pages" id="basic-nav-dropdown">
-             // <NavDropdown.Item as={Link} to="/action">Action</NavDropdown.Item>
-             // <NavDropdown.Item as={Link} to="/another-action">Another Action</NavDropdown.Item>
-              //<NavDropdown.Item as={Link} to="/something">Something</NavDropdown.Item>
-              //<NavDropdown.Divider />
-             // <NavDropdown.Item as={Link} to="/separated">Separated Link</NavDropdown.Item>
-            //</NavDropdown>*/
-
+            <Nav.Link as={Link} to="/" className={styles.navLink}>
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about" className={styles.navLink}>
+              About
+            </Nav.Link>
+            <NavDropdown title="Pages" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/action" className={styles.dropdownItem}>
+                Action
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/another-action" className={styles.dropdownItem}>
+                Another Action
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/something" className={styles.dropdownItem}>
+                Something
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} to="/separated" className={styles.dropdownItem}>
+                Separated Link
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
